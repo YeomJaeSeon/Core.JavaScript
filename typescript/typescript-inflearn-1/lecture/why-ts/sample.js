e27446e374a8de5624ba9bcfb95a8eda76442197
YeomJaeSeon <a89541457@gmail.com>
2022-04-11 20:16:50 +0900

    ts쓰면 좋은점

---
+++ typescript/typescript-inflearn-1/lecture/why-ts/sample.js
@@ -0,0 +1,8 @@
+function sum(a, b){
+    return a + b;
+}
+
+console.log(sum(10, '20'))
+
+var result = sum(1, 2)
+// result.toLocalString()
---
+++ typescript/typescript-inflearn-1/lecture/why-ts/sample.ts
@@ -0,0 +1,6 @@
+function sum2(a: number, b: number){
+    return a + b;
+}
+
+var result2 = sum2(1, 2)
+result2.toLocaleString()
