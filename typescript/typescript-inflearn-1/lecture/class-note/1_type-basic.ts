c4757b1dbbecf385e024d375b574cebcc09ede91
YeomJaeSeon <a89541457@gmail.com>
2022-04-12 23:45:41 +0900

    기본타입2 - 튜플, 객체(객체의 프로퍼티도 타입지정가능), boolean

--- typescript/typescript-inflearn-1/lecture/class-note/1_type-basic.ts
+++ typescript/typescript-inflearn-1/lecture/class-note/1_type-basic.ts
@@ -11,3 +11,23 @@ let num: number = 10 // 변수의 타입 선언하는건 이렇게! 쉽다
 let arr: Array<number> = [1, 2 ,3]
 let heroes: Array<string> = ['슈퍼맨', '백터맨', '아이언맨']
 let items: number[] = [1, 2, 3] //배열 리터럴로 변수 타입선언가능
+
+// TS 튜플
+// 배열의 각각 인덱스의 타입도 지정할수있음 
+let address: [string, number, number] = ['gangnam', 1, 3]
+
+// TS 객체
+let obj: object = {}
+let person: object= {
+    name: 'capt',
+    age: 100
+}
+
+// 객체의 프로퍼티의 타입도 지정해줄수있음(튜플과 같이)
+let improvedPerson: { name: string, age: number} = {
+    name: '염재선',
+    age: 5
+}
+
+// TS 진위값(boolean)
+let show: boolean = true
