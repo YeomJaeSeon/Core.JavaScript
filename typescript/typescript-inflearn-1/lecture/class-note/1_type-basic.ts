d3a6f0fa7d33fdcfff9a2f82a10fc9fd0bbcacbd
YeomJaeSeon <a89541457@gmail.com>
2022-04-12 23:39:04 +0900

    문자열, 숫자, 배열 타입지정해보기

---
+++ typescript/typescript-inflearn-1/lecture/class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+//JS 문자열선언
+var str = '안녕'
+
+//TS 문자열 선언
+var str: string = 'hello'
+
+//TS 숫자
+let num: number = 10 // 변수의 타입 선언하는건 이렇게! 쉽다
+
+//TS 배열
+let arr: Array<number> = [1, 2 ,3]
+let heroes: Array<string> = ['슈퍼맨', '백터맨', '아이언맨']
+let items: number[] = [1, 2, 3] //배열 리터럴로 변수 타입선언가능
