0e69cfbd2e23d8e5d46d65ea637346d49efb22c0
YeomJaeSeon <a89541457@gmail.com>
2022-04-12 23:59:27 +0900

    if, ? 조건처리

---
+++ JavaScriptInfo/playground/if_?.js
@@ -0,0 +1,13 @@
+if(0){
+    // if() 괄호안의 표현식 평가후 불린값으로 반환
+    console.log(0)
+}
+if(1){
+    console.log(1)
+}
+
+const year = 2014
+let cond = (year == 2015)
+if(cond){
+    console.log('cond')
+}
