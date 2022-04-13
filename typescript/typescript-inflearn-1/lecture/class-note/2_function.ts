818aaa540f74a52c7c3f56250f42191a6c3a237d
YeomJaeSeon <a89541457@gmail.com>
2022-04-13 23:04:56 +0900

    타스 사용하면 함수의 파라미터 엄격

--- typescript/typescript-inflearn-1/lecture/class-note/2_function.ts
+++ typescript/typescript-inflearn-1/lecture/class-note/2_function.ts
@@ -1,5 +1,5 @@
 //함수 파라미터에 타입 정의
-function sum(a: number, b: string){
+function sum2(a: number, b: number){
     return a + b;
 }
 
@@ -7,3 +7,5 @@ function sum(a: number, b: string){
 function add(): number{
     return 10;
 }
+
+sum2(1, 2, 3, 4) 
---
+++ typescript/typescript-inflearn-1/lecture/class-note/function.js
@@ -0,0 +1,6 @@
+function sum(a, b){
+    console.log(arguments)
+    return a + b;
+}
+
+sum(1, 2, 3, 4, 5)
