6b5134dc889de51850afec091bd5f5778de0b290
YeomJaeSeon <a89541457@gmail.com>
2022-05-11 22:44:06 +0900

    자바스크립트: split, join

--- JavaScriptInfo/playground/array_methods.js
+++ JavaScriptInfo/playground/array_methods.js
@@ -32,7 +32,30 @@ console.log(arr)
 arr.forEach((value, idx, arr)=> {
     console.log(arr)
     console.log('idx', idx)
-    console.log("value", value다
+    console.log("value", value);
 })
 
 // arr.reverse()
+
+
+let names = '염재선, 염민선, 하하, 정준하';
+let nameArr = names.split(', ')
+let familyNameArr = names.split(', ', 2); // 두번째 인자는 배열의 길이 제한
+
+console.log(nameArr);
+console.log(familyNameArr)
+
+let str = 'hello'
+let strArr = str.split('')
+console.log(strArr)
+for(let value of strArr){
+    console.log(value)
+}
+
+arr = ['염재선', '염민선', '정준하', '하하'];
+const arrStr = arr.join('_');
+console.log(arrStr)
+
+arr.reduce(function(accumulator, item, idx, array){
+
+})
